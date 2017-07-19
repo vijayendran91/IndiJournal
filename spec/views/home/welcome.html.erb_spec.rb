aa786ce7ccdbbfed94e0d2da0a30350772e36156
Vijayendran <vijayendran91@gmail.com>
2017-07-19 15:56:16 -0400

    First Message

---
+++ spec/views/home/welcome.html.erb_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "home/welcome.html.erb", type: :view do
+  scenario "Home page should welcome" do
+    visit "/"
+    expect(page).to have_content("Welcome to Independant Journalism")
+  end
+end
---
+++ test/controllers/home_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+Rspec.describe Home, :type => :controller do
+
+
+end
